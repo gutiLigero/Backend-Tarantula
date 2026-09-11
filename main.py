3c1cd498e48f89f96e04b3e0bd2bf824ad93b4e6
Juan <125102252+gutiLigero@users.noreply.github.com>
2026-09-11 11:18:19 -0500

    Enable credentials in CORS settings

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.add_middleware(
         "http://localhost:8000", 
         "http://127.0.0.1:8000"
     ], 
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
